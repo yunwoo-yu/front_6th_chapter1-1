b5216e833b4de07c794a3a35704a36ebc13a771d
Ycow <skypnal12@gmail.com>
2025-07-12 02:31:11 +0900

    feat: 배포테스트 5

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -1,11 +1,11 @@
 import { setupWorker } from "msw/browser";
 import { handlers } from "./handlers";
 
+// MSW 워커 설정
 const basePath = import.meta.env.PROD ? "/front_6th_chapter1-1" : "";
 
 export const worker = setupWorker(...handlers);
 
-// Worker start 옵션을 export하여 main.js에서 사용
 export const workerOptions = import.meta.env.PROD
   ? {
       serviceWorker: {
