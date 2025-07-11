9979db1c4738bbdb11a945a9685a38c0c9d15c38
Ycow <skypnal12@gmail.com>
2025-07-12 02:48:39 +0900

    feat: 배포테스트 9

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -6,6 +6,7 @@ const basePath = import.meta.env.PROD ? "/front_6th_chapter1-1" : "";
 
 export const worker = setupWorker(...handlers);
 
+// Worker start 옵션을 export하여 main.js에서 사용
 export const workerOptions = import.meta.env.PROD
   ? {
       serviceWorker: {
