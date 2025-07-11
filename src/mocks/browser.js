bcf71d5ae5311576980861063c592704c2248bd1
Ycow <skypnal12@gmail.com>
2025-07-12 02:41:24 +0900

    feat: 배포테스트 9

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -2,8 +2,17 @@ import { setupWorker } from "msw/browser";
 import { handlers } from "./handlers";
 
 // MSW 워커 설정
+const basePath = import.meta.env.PROD ? "/front_6th_chapter1-1" : "";
+
 export const worker = setupWorker(...handlers);
 
-export const workerOptions = {
-  onUnhandledRequest: "bypass",
-};
+export const workerOptions = import.meta.env.PROD
+  ? {
+      serviceWorker: {
+        url: `${basePath}/mockServiceWorker.js`,
+      },
+      onUnhandledRequest: "bypass",
+    }
+  : {
+      onUnhandledRequest: "bypass",
+    };
